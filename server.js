581cf001b460a280fdd0e2177cd0c47d7015e7bf
alpunver <alpunverr@gmail.com>
2025-12-11 16:46:49 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const ADMIN_PASSWORD = "123";
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 // --- ROTALAR ---
 
